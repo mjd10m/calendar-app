073069a955b875b6d4302779287f30eb33a274c1
mjd10m <mjd10m@outlook.com>
2022-07-14 00:27:43 -0400

    js for adding and editing tasks and momentjs

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,95 @@
+var tasks = {}
+var taskStorage = []
+var elementEdit = '';
+var textBoxInput = '';
+var currentDate = moment().format('ddd MMMM Do');
+
+function checkTime() {
+$(".time-container .activity-container").each(function(index, el) {
+    auditTask(el);
+  });
+}
+
+setInterval(checkTime(), 10000);
+
+$("#currentDay").text(currentDate)
 
 $(".activity-container").on("click", ".activity", function() {
     console.log("clicked")
+    elementEdit = $(this);
     var text = $(this)
    .text()
    .trim();
-   var textInput = $("<textarea>")
-   .addClass("form-control")
+   textBoxInput = $("<textarea>")
+   .addClass("form-control col-7")
    .val(text);
-   $(this).replaceWith(textInput);
-   textInput.trigger("focus");
-   });
+   $(this).replaceWith(textBoxInput);
+    textBoxInput.trigger("focus");
+    $(".activity-container").css("pointer-events", "none")
+    $(".saveBtn").css("pointer-events", "auto")
 
+});
+//$(".activity-container").on("blur","textarea",function(){
+
+//})
 
 $(".time-container").on("click", ".activity-container", function() {
-   console.log("clicked")
-  var textInput = $("<textarea>")
-  .addClass("form-control col-10")
-  $(this).append(textInput);
-  textInput.trigger("focus");
-  });
+    console.log("clicked");
+    var textInput = $("<textarea>").addClass("form-control col-7");
+    $(this).append(textInput);
+    textInput.trigger("focus");
+    $(".activity-container").css("pointer-events", "none")
+    $(".saveBtn").css("pointer-events", "auto")
+});
+
+$(".time-container").on("click",".saveBtn", function() {
+    var taskText = $("textarea").val().trim();
+    var taskContainer = $(this)
+    taskContainer = taskContainer.prev()
+    createTask(taskText, taskContainer, elementEdit, textBoxInput)
+    $("textarea").remove()
+    $(".activity-container").css("pointer-events", "auto")
+    $(".saveBtn").css("pointer-events", "none")
+    elementEdit = ''
+    textBoxInput = ''
+    
+});
+
+function createTask(taskText, taskContainer, elementEdit, textBoxInput) {
+    var taskDiv = $("<div>").addClass("activity").attr("id", taskNumber);
+    var taskId = $(taskContainer).attr("id")
+    var taskNumber = $(".activity").length
+    if (elementEdit === '') {
+    taskDiv.text(taskText);
+    taskContainer.append(taskDiv)
+    console.log(taskId);
+    console.log(taskText);
+    } else {
+
+        elementEdit.text(taskText);
+        textBoxInput.replaceWith(elementEdit);
+    }
+}
+
+function auditTask(task) {
+    var timeOffset = $(task).attr("id")
+    var time = moment().format("H")
+    $(task).removeClass("past present future");
+    if (time > timeOffset + 8) {
+        $(task).addClass("past");
+      } else if (time === timeOffset + 8) {
+        $(task).addClass("present");
+      } else {
+        $(task).addClass("future");
+      }
+    console.log(time);
+}
+
+function load() {
+    $(".saveBtn").css("pointer-events", "none")
+}
+load()
+
+
+  
+  
