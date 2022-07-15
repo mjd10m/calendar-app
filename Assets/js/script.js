b1a8f695c061e37e26f7c8e5bd96f97b366ceb29
mjd10m <mjd10m@outlook.com>
2022-07-15 00:14:15 -0400

    comments and readME

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,8 @@
-var taskStorage = []
+var taskStorage = [];
 var elementEdit = '';
 var textBoxInput = '';
 var currentDate = moment().format('ddd MMMM Do');
-
+//checks the current time and logs current time to console
 function checkTime() {
 $(".time-container .activity-container").each(function(index, el) {
     auditTask(el);
@@ -12,79 +12,75 @@ $(".time-container .activity-container").each(function(index, el) {
 
 setInterval(checkTime, 60000);
 
-$("#currentDay").text(currentDate)
-
+$("#currentDay").text(currentDate);
+//open textarea when a time slot is clicked
 function openTextBox(clickedEl) {
 
     textBoxInput = $("<textarea>").addClass("form-control col-7");
     $(clickedEl).append(textBoxInput);
     textBoxInput.trigger("focus");
-    $(".saveBtn").css("pointer-events", "auto")
+    $(".saveBtn").css("pointer-events", "auto");
 }
+//opens textarea when an task is clicked
 function openTextBoxEdit(clickedEl) {
     elementEdit = clickedEl;
-    var text = $(clickedEl)
-   .text()
-   .trim();
-   textBoxInput = $("<textarea>")
-   .addClass("form-control col-7")
-   .val(text);
+    var text = $(clickedEl).text().trim();
+   textBoxInput = $("<textarea>").addClass("form-control col-7").val(text);
    $(clickedEl).replaceWith(textBoxInput);
     textBoxInput.trigger("focus");
 }
-
+//saves tasks from the temporary textarea
 function saveTask(clickedEl) {
     var taskText = $("textarea").val().trim();
-    var taskContainer = $(clickedEl).prev()
+    var taskContainer = $(clickedEl).prev();
     var clickedElClass = $(clickedEl)[0].classList[0];
-    createTask(taskText, taskContainer, elementEdit, textBoxInput, clickedElClass)
-    $("textarea").remove()
-    $(".saveBtn").css("pointer-events", "none")
-    elementEdit = ''
-    textBoxInput = ''
-    localStorage.setItem("tasks", JSON.stringify(taskStorage))
+    createTask(taskText, taskContainer, elementEdit, textBoxInput, clickedElClass);
+    $("textarea").remove();
+    $(".saveBtn").css("pointer-events", "none");
+    elementEdit = '';
+    textBoxInput = '';
+    localStorage.setItem("tasks", JSON.stringify(taskStorage));
 }
-
+//handles all click events on the page and guides to the appropiate function 
 $(document).on("click",function(event){
-    var activityContainerClass = $(".activity-container")[0].classList[0]
-    var activityClass = $(".activity").attr("class")
-    var saveBtnClass = $(".saveBtn")[0].classList[0]
-    var textAreaClass = $("textarea").attr("class")
-    console.log(event.target)
-    console.log(textAreaClass)
+    var activityContainerClass = $(".activity-container")[0].classList[0];
+    var activityClass = $(".activity").attr("class");
+    var saveBtnClass = $(".saveBtn")[0].classList[0];
+    var textAreaClass = $("textarea").attr("class");
+    console.log(event.target);
+    console.log(textAreaClass);
     var clickedEl = event.target;
     var clickedElClass = $(clickedEl)[0].classList[0];
     if(clickedElClass === activityContainerClass && textAreaClass === undefined) {
-        openTextBox(clickedEl)
-        $(".saveBtn").css("pointer-events", "auto")
+        openTextBox(clickedEl);
+        $(".saveBtn").css("pointer-events", "auto");
     } else if(clickedElClass === activityClass && textAreaClass === undefined) {
         openTextBoxEdit(clickedEl);
-        $(".saveBtn").css("pointer-events", "auto")
+        $(".saveBtn").css("pointer-events", "auto");
     } else if(clickedElClass === saveBtnClass) {
-        saveTask(clickedEl)
+        saveTask(clickedEl);
     } else if(!(textAreaClass === undefined)) {
         if(elementEdit === '') {
-            $("textarea").remove()
+            $("textarea").remove();
         } else {
             var index = $(elementEdit).attr("id");
-            var taskobj = taskStorage[index]
-            var taskText = taskobj.text
-            debugger;
+            var taskobj = taskStorage[index];
+            var taskText = taskobj.text;
             $(elementEdit).text(taskText);
             $(textBoxInput).replaceWith(elementEdit);
-            elementEdit = ''
-            textBoxInput = ''
+            elementEdit = '';
+            textBoxInput = '';
         }
     }
 })
-
+//creates the div element to store task
 function createTask(taskText, taskContainer, elementEdit, textBoxInput, clickedElClass) {
-    var taskNumber = $(".activity").length
+    var taskNumber = $(".activity").length;
     var taskDiv = $("<div>").addClass("activity").attr("id", taskNumber);
-    var taskId = $(taskContainer).attr("id")
+    var taskId = $(taskContainer).attr("id");
     if (elementEdit === '') {
     taskDiv.text(taskText);
-    taskContainer.append(taskDiv)
+    taskContainer.append(taskDiv);
     taskStorage.push({
         text: taskText,
         id: taskId
@@ -93,15 +89,16 @@ function createTask(taskText, taskContainer, elementEdit, textBoxInput, clickedE
         var index = $(elementEdit).attr("id");
         var taskobj = taskStorage.splice(index, 1);
         taskobj.text = taskText;
-        taskStorage.splice(index, 1, taskobj)
+        debugger;
+        taskStorage.splice(index, 1, taskobj);
         $(elementEdit).text(taskText);
         $(textBoxInput).replaceWith(elementEdit);
     }
 }
-
+//updates the columns color when hour changes
 function auditTask(task) {
-    var timeOffset = parseInt($(task).attr("id"))
-    var time = parseInt(moment().format("H"))
+    var timeOffset = parseInt($(task).attr("id"));
+    var time = parseInt(moment().format("H"));
     $(task).removeClass("past present future");
     if (time > timeOffset + 8) {
         $(task).addClass("past");
@@ -111,21 +108,21 @@ function auditTask(task) {
         $(task).addClass("future");
       }
 }
-
+//retrieves saved tasks from local storage and publishes to page
 function loadStoredTasks() {
     var recalledTasks = JSON.parse(localStorage.getItem("tasks"));
     recalledTasks.forEach((obj, index) => {
-        var taskText = obj.text
+        var taskText = obj.text;
         var containerClassId = ".activity-container" + "#" + obj.id
-        var taskContainer = $(containerClassId)
-        createTask(taskText, taskContainer, elementEdit, textBoxInput)
+        var taskContainer = $(containerClassId);
+        createTask(taskText, taskContainer, elementEdit, textBoxInput);
     })
 }
-
+//function to run when page is loaded
 function load() {
-    $(".saveBtn").css("pointer-events", "none")
+    $(".saveBtn").css("pointer-events", "none");
     checkTime();
-    loadStoredTasks()
+    loadStoredTasks();
 }
 load()
 
